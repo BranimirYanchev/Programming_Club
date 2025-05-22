a29cbc0b54fbde21f86c32c3247e20c6a3b06fd8
Spas Kitanov <kitanovspas08@gmail.com>
2025-05-22 11:30:11 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__ . '../Programming_Club/PHPMailer/src/Exception.php';
-require __DIR__ . '../PHPMailer/src/PHPMailer.php';
-require __DIR__ . '../PHPMailer/src/SMTP.php';
+require __DIR__ . '../var/www/html/Programming_Club/PHPMailer/src/Exception.php';
+require __DIR__ . '../var/www/html/Programming_Club/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '../var/www/html/Programming_Club/PHPMailer/src/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
